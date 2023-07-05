acb6d3ed3bfe8ea8e37d0ca3ffbc6f45eb692a90
Xiao <79354981+owlxiao@users.noreply.github.com>
2023-07-05 20:15:27 +0800

    feat: Add detector startup in bringup launch file

--- launch/fcs_bringup.launch.py
+++ launch/fcs_bringup.launch.py
@@ -3,35 +3,50 @@ import os
 from launch import LaunchDescription
 from launch_ros.descriptions import ComposableNode
 from ament_index_python.packages import get_package_share_directory
-from launch_ros.actions import Node
-from launch.actions import GroupAction
+from launch_ros.actions import ComposableNodeContainer, Node
+from launch.actions import GroupAction, Shutdown
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
+    def get_camera_node(package, plugin):
+        package_dir = get_package_share_directory('ros2_astra_camera')
+        parameters_path = os.path.join(
+            package_dir, 'launch', 'params', 'astra_pro.yaml')
 
-    package_dir = get_package_share_directory('ros2_astra_camera')
-    parameters_path = os.path.join(
-        package_dir, 'launch', 'params', 'astra_pro.yaml')
+        return ComposableNode(
+            package=package,
+            plugin=plugin,
+            name='camera_node',
+            parameters=[parameters_path],
 
-    start_astra_camera_node_cmd = Node(
-        package="ros2_astra_camera",
-        executable="astra_camera_node",
-        name="astra_camera_node",
-        parameters=[parameters_path]
-    )
+        )
 
-    start_uvc_camera_node_cmd = Node(
-        package="ros2_astra_camera",
-        executable="uvc_camera_node",
-        name="uvc_camera_node",
-        output="screen",
-        emulate_tty=True,
-        parameters=[parameters_path]
-    )
+    def get_camera_detector_container(camera_node):
+        return ComposableNodeContainer(
+            name='camera_detector_container',
+            namespace='',
+            package='rclcpp_components',
+            executable='component_container',
+            composable_node_descriptions=[
+                camera_node,
+                ComposableNode(
+                    package='detector',
+                    plugin='rt_vision::DetectorNode',
+                    name='detector',
+                )
+            ],
+            output='both',
+            emulate_tty=True,
+            on_exit=Shutdown(),
+        )
+
+    astra_camera_node = get_camera_node(
+        'ros2_astra_camera', 'ros2_astra_camera::CameraDriver')
+
+    cam_detector = get_camera_detector_container(astra_camera_node)
 
     return LaunchDescription([
-        start_astra_camera_node_cmd,
-        start_uvc_camera_node_cmd
+        cam_detector
     ])
